d7e7d681d8ab7f64c95ebeade440bfd35371fb8c
Kiersten Campbell <55903462+EternalStorm4444@users.noreply.github.com>
2020-10-24 20:15:06 -0400

    Add files via upload
    skeleton of locations and items - need update descriptions

---
+++ locations_and_items.py
@@ -0,0 +1,26 @@
+#locations and item function descriptions
+
+def medbay():
+    print('medbay description')
+
+def alt_crew_quarters():
+    print('description')
+
+def your_quarters():
+    print("description")
+
+def engines():
+    print('description')
+
+def storage():
+    print('description')
+
+def reactor():
+    print('description')
+
+def oxygen():
+    print('description')
+
+def cockpit():
+    print('description')
+
