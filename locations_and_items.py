6aa767c09b65eff4aad55857c8f2a6c8755fa263
Oliver Keen <72150727+oliverkeen@users.noreply.github.com>
2021-01-25 20:07:20 -0500

    Add files via upload

--- locations_and_items.py
+++ locations_and_items.py
@@ -1,31 +1,46 @@
 # Locations and item function descriptions
 
 def medbay():
-    print("You are standing in the medbay. Behind you, a door leads back to the cafeteria.")
+    print("You are standing in the medbay. Behind you, a door leads back to "
+          "the cafeteria.")
 
 def cafeteria(): ########### Missing in other file, needs added
-    print("You are in the middle of the cafeteria. Ahead, there is a door to the oxygen \nroom, and above you is the cockpit entrance. To your left is the medbay door, \nand to your right is the crew's quarters. Behind you, there is an entrance to \nyour personal quarters.")
+    print("You are in the middle of the cafeteria. Ahead, there is a door to "
+          "the oxygen \nroom, and above you is the cockpit entrance. To your "
+          "left is the medbay door, \nand to your right is the crew's "
+          "quarters. Behind you, there is an entrance to \nyour personal"
+          "quarters.")
 
 def crew_quarters(): ### make sure names match
-    print("You are standing in the crew's quarters. The door ahead leads to the reactor \nroom, and on your right is a storage entrance. Behind you, a door leads to the \ncafeteria.")
+    print("You are standing in the crew's quarters. The door ahead leads to "
+          "the reactor \nroom, and on your right is a storage entrance. Behind "
+          "you, a door leads to the \ncafeteria.")
 
 def your_quarters(): ### make sure names match
-    print("You are in your personal quarters. The engine room lays after the door in front \nof you, and on your left is a storage entrance. A doorway to the cafeteria is \nbehind you.")
+    print("You are in your personal quarters. The engine room lays after the "
+          "door in front \nof you, and on your left is a storage entrance. A "
+          "doorway to the cafeteria is \nbehind you.")
 
 def engines():
-    print("You are standing in the engine room. The door behind you leads back to your \nquarters.")
+    print("You are standing in the engine room. The door behind you leads back "
+          "to your \nquarters.")
 
 def storage():
-    print("You are standing in storage. On your left is an entrance to your personal \nquarters, and to your right is an entrance to the crew's quarters.")
+    print("You are standing in storage. On your left is an entrance to your "
+          "personal \nquarters, and to your right is an entrance to the crew's "
+          "quarters.")
 
 def reactor():
-    print("You are standing in the reactor room. Behind you, a door leads back to the \ncrew's quarters.")
+    print("You are standing in the reactor room. Behind you, a door leads back "
+          "to the \ncrew's quarters.")
 
 def oxygen():
-    print("You are standing in the oxygen room. The door behind you leads back to the \ncafeteria.")
+    print("You are standing in the oxygen room. The door behind you leads back "
+          "to the \ncafeteria.")
 
 def cockpit():
-    print("You are in the ship's cockpit. The hatch to the cafeteria lays below you.")
+    print("You are in the ship's cockpit. The hatch to the cafeteria lays "
+          "below you.")
 
 """
 ### Output testing
